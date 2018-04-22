53b944a1448a43687956e628be4330d39651dc33
Dmitriy Gavrilov <d_man_sun@mail.ru>
2018-04-22 15:24:28 +0600

    Top mobile menu is ready

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,75 @@
+import {isMobileView, goToUrl} from './common-functions'
+
 $( document ).ready(function() {
 
   /** Mobile menu button */
-  $( ".js-hamburger" ).on( 'click', function () {
 
-    $( this ).toggleClass( 'is-active' );
+  const headerMobileMenuToggleButton = $( ".js-hamburger" );
 
-  });
+  (function ($) {
+
+    const headerMobileMenu = $('#header-mobile-menu');
+
+    if( !headerMobileMenuToggleButton.length ) return false;
+
+    headerMobileMenuToggleButton.on( 'click', function () {
+
+      const $this = $( this );
+      const activeClass = 'is-active';
+
+      if( headerMobileMenu.css('display') === 'none' ) {
+        headerMobileMenu.css({ 'display' : 'flex' });
+        $this.addClass( activeClass );
+      } else {
+        headerMobileMenu.css({ 'display' : 'none' });
+        $this.removeClass( activeClass );
+      }
+
+    });
+
+  })($);
+
+
+  /** Main top menu ( mobile ) */
+
+  (function ($) {
+
+    const topMainMenuClass = 'js-header-main-menu__root-menu';
+    const topMainMenuSelector = '.' + topMainMenuClass;
+    const topMainMenuItemWithSubmenuClass = 'has-sub-menu';
+    const topMainMenuItemWithSubmenuSelector = 'li.' + topMainMenuItemWithSubmenuClass;
+    const topMainMenuItemLink = $(topMainMenuSelector + ' li.has-sub-menu a');
+    const subMenuOpenClass = 'sub-menu-is-open';
+
+    topMainMenuItemLink.on( 'click', function (e) {
+
+      const $this = $(this);
+      const linkUrl = $(this).attr( 'href' );
+
+      if( !isMobileView() ) {
+        goToUrl( linkUrl );
+        return false;
+      }
+
+      e.preventDefault();
+
+      const menuListItem = $this.closest( 'li' );
+
+      if( !menuListItem.length || !menuListItem.hasClass( topMainMenuItemWithSubmenuClass ) ) {
+        goToUrl( linkUrl );
+        return false;
+      }
+
+      const subMenu = menuListItem.children( 'ul' );
+
+      menuListItem.toggleClass(subMenuOpenClass);
+      subMenu.slideToggle();
+
+    });
+
+  })($)
+
+
 
 
 
