643a448c1259c5889f9178a60a45d6908c848649
Dmitriy Gavrilov <d_man_sun@mail.ru>
2018-04-22 17:26:23 +0600

    Header search is ready

--- src/js/app.js
+++ src/js/app.js
@@ -67,7 +67,64 @@ $( document ).ready(function() {
 
     });
 
-  })($)
+  })($);
+
+  /** Top search form */
+
+  (function ($) {
+
+    // Show/hide search form
+
+    const searchFrom = $( '#top-search-form' );
+
+    $( '.js-show-top-search' ).on( 'click', function () {
+
+      setTimeout( () => {
+        searchFrom.css({ 'display' : 'block' });
+      }, 10 );
+
+    });
+
+    $(document).on('click', function(event) {
+
+      if ( !$(event.target).closest('#top-search-form').length ) {
+
+        if( searchFrom.css('display') === 'block' ) {
+          searchFrom.css({ 'display' : 'none' });
+        }
+
+      }
+    });
+
+    // Cross browser placeholder color
+
+    const searchInput = $( '#top-search-input' );
+    const searchInputWrap = searchInput.closest( '.js-top-search-form__input-wrap' );
+    const searchInputPlaceholder = $( searchInputWrap.find('.js-top-search-form__input-placeholder')[0] );
+
+    searchInput.on( 'focus', function () {
+
+      if( !searchInputPlaceholder.length ) return false;
+
+      searchInputPlaceholder.css({ 'display' : 'none' });
+
+    });
+
+    searchInput.on( 'blur', function () {
+
+      if( !searchInputPlaceholder.length ) return false;
+
+      const $this = $(this);
+      const value = $this.val();
+
+      if( !value.length ) {
+        searchInputPlaceholder.css({ 'display' : 'block' });
+      }
+
+    });
+
+  })($);
+
 
 
 
